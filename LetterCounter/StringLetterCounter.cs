c4d09505ff2ef8a9f095de11869420f38936227e
Sandro <sherashera777@gmail.com>
2024-04-12 00:28:52 +0400

    Add project files.

---
+++ LetterCounter/StringLetterCounter.cs
@@ -0,0 +1,207 @@
+﻿namespace LetterCounter
+{
+    public static class StringLetterCounter
+    {
+        public static int UnequalConsecutiveLettersCounter(string input)
+        {
+            int count = 0;
+            int innerCount = 0;
+
+            if (input is null)
+            {
+                throw new ArgumentNullException("input");
+            }
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentException("input");
+            }
+
+            if (input.Length == 1 && char.IsLetter(input[0]))
+            {
+                return 1;
+            }
+
+            for (int i = 0; i < input.Length - 1; i ++)
+            {
+                if (!char.IsLetter(input[i]))
+                {
+                    innerCount = 0;
+
+                    continue;
+                }
+
+                if (!char.IsLetter(input[i + 1]))
+                {
+                    if(innerCount > count)
+                    {
+                        count = innerCount;
+                    }
+
+                    innerCount = 0;
+
+                    continue;
+                }
+
+                if (input[i] != input[i + 1])
+                {
+                    if (innerCount == 0)
+                    {
+                        innerCount += 2;
+                    }
+                    else
+                    {
+                        innerCount++;
+                    }
+
+                    if (innerCount > count)
+                    {
+                        count = innerCount;
+                    }
+                }
+                else
+                {
+
+                    innerCount = 0;
+                }
+            }
+
+            return count;
+        }
+
+        public static int EqualConsecutiveLettersCounter(string input)
+        {
+            int count = 0;
+            int innerCount = 0;
+
+            if (input is null)
+            {
+                throw new ArgumentNullException("input");
+            }
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentException("input");
+            }
+
+            if (input.Length == 1 && char.IsLetter(input[0]))
+            {
+                return 1;
+            }
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (!char.IsLetter(input[i]))
+                {
+                    innerCount = 0;
+
+                    continue;
+                }
+
+                if (!char.IsLetter(input[i + 1]))
+                {
+                    if (innerCount > count)
+                    {
+                        count = innerCount;
+                    }
+
+                    innerCount = 0;
+
+                    continue;
+                }
+
+                if (input[i] == input[i + 1])
+                {
+                    if (innerCount == 0)
+                    {
+                        innerCount += 2;
+                    }
+                    else
+                    {
+                        innerCount++;
+                    }
+
+                    if (innerCount > count)
+                    {
+                        count = innerCount;
+                    }
+                }
+                else
+                {
+
+                    innerCount = 0;
+                }
+            }
+
+            return count;
+        }
+
+        public static int EqualConsecutiveDigitsCounter(string input)
+        {
+            int count = 0;
+            int innerCount = 0;
+
+            if (input is null)
+            {
+                throw new ArgumentNullException("input");
+            }
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentException("input");
+            }
+
+            if (input.Length == 1 && char.IsDigit(input[0]))
+            {
+                return 1;
+            }
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (!char.IsDigit(input[i]))
+                {
+                    innerCount = 0;
+
+                    continue;
+                }
+
+                if (!char.IsDigit(input[i + 1]))
+                {
+                    if (innerCount > count)
+                    {
+                        count = innerCount;
+                    }
+
+                    innerCount = 0;
+
+                    continue;
+                }
+
+                if (input[i] == input[i + 1])
+                {
+                    if (innerCount == 0)
+                    {
+                        innerCount += 2;
+                    }
+                    else
+                    {
+                        innerCount++;
+                    }
+
+                    if (innerCount > count)
+                    {
+                        count = innerCount;
+                    }
+                }
+                else
+                {
+
+                    innerCount = 0;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
---
+++ LetterCounterTests/StringLetterCounterTests.cs
@@ -0,0 +1,82 @@
+﻿using LetterCounter;
+
+namespace LetterCounterTests
+{
+    internal class StringLetterCounterTests
+    {
+        [TestCase("aaaaaaaaaaaa", 0)]
+        [TestCase("12$4567*(@_=", 0)]
+        [TestCase("1", 0)]
+        [TestCase("a", 1)]
+        [TestCase("aabbccddeeffgg", 2)]
+        [TestCase("abc", 3)]
+        [TestCase("a1abc!", 3)]
+        [TestCase("aaVVvBBBZZasww", 4)]
+        [TestCase("abcdefghijklll", 12)]
+        public void UnequalConsecutiveLettersCounterValidInput(string input, int expected)
+        {
+            var actual = StringLetterCounter.UnequalConsecutiveLettersCounter(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void UnequalConsecutiveLettersCounterThrowsArgumentNullException()
+        {
+            string? x = null;
+            Assert.Throws<ArgumentNullException>(() => StringLetterCounter.UnequalConsecutiveLettersCounter(x!));
+        }
+
+        [TestCase("")]
+        [TestCase("      ")]
+        [Test]
+        public void UnequalConsecutiveLettersCounterThrowsArgumentException(string input)
+        {
+            Assert.Throws<ArgumentException>(() => StringLetterCounter.UnequalConsecutiveLettersCounter(input));
+        }
+
+        [TestCase("1", 0)]
+        [TestCase("abcdefg", 0)]
+        [TestCase("12$4567*(@_=", 0)]
+        [TestCase("a", 1)]
+        [TestCase("aabbccddeeffgg", 2)]
+        [TestCase("aaa", 3)]
+        [TestCase("a1abbbc$!", 3)]
+        public void EqualConsecutiveLettersCounterValidInput(string input, int expected)
+        {
+            var actual = StringLetterCounter.EqualConsecutiveLettersCounter(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void EqualConsecutiveLettersCounterThrowsArgumentNullException()
+        {
+            string? x = null;
+            Assert.Throws<ArgumentNullException>(() => StringLetterCounter.EqualConsecutiveLettersCounter(x!));
+        }
+
+        [TestCase("")]
+        [TestCase("      ")]
+        [Test]
+        public void EqualConsecutiveLettersCounterThrowsArgumentException(string input)
+        {
+            Assert.Throws<ArgumentException>(() => StringLetterCounter.UnequalConsecutiveLettersCounter(input));
+        }
+
+        [TestCase("!", 0)]
+        [TestCase("abcdefg", 0)]
+        [TestCase("12345", 0)]
+        [TestCase("ab$cdef*(@_=", 0)]
+        [TestCase("1", 1)]
+        [TestCase("11223344556677", 2)]
+        [TestCase("111", 3)]
+        [TestCase("1a1222c$!", 3)]
+        public void EqualConsecutiveDigitsCounterValidInput(string input, int expected)
+        {
+            var actual = StringLetterCounter.EqualConsecutiveDigitsCounter(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
